36970170e82ab99fcd923fa1891ddcdb9e4b20a1
Sidratul Islam <sidratul@topofstacksoftware.com>
2023-09-09 21:42:01 +0600

    test error

--- src/app/utility/MiddlewareUtility.js
+++ src/app/utility/MiddlewareUtility.js
@@ -14,7 +14,8 @@ export async function CheckCookieAuth(req) {
         })
     }
     catch (e) {
-        return NextResponse.redirect(new URL('/login', req.url))
+        return NextResponse.json({status:false,message:JSON.stringify(e)})
+        // return NextResponse.redirect(new URL('/login', req.url))
     }
 
 
